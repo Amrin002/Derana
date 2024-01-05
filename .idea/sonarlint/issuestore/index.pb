5cfb78c4ec7393ca4895851724c6c474e212d061
Amrin002 <amrinlatuconsina@gmail.com>
2024-01-05 18:53:11 +0900

    Splash Screen di Tambahkan

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,3 +1,7 @@
 
 l
 <app/src/main/java/com/deranaindonesia/derana/MainActivity.kt,b\8\b8148432e439e5acc28a001fd2f39e1767b6aba1
+n
+>app/src/main/java/com/deranaindonesia/derana/SplashActivity.kt,a\7\a71afdb5c89b21c27f7aaf43c23bc54dc492e99f
+P
+ app/src/main/AndroidManifest.xml,8\c\8c55c3ccc257e5907959013f99656e4c8ec3903e
---
+++ app/src/main/java/com/deranaindonesia/derana/SplashActivity.kt
@@ -0,0 +1,30 @@
+package com.deranaindonesia.derana
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ProgressBar
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+    private lateinit var loadingbar : ProgressBar
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        loadingbar = findViewById(R.id.progressBar)
+        val thread= Thread{
+            try{
+                Thread.sleep(1000)
+            } catch(e: InterruptedException){
+                e.printStackTrace()
+            }finally {
+                startActivity(Intent(this, MainActivity::class.java))
+                finish()
+            }
+        }
+        thread.start()
+    }
+}
