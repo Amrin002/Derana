1829e8bcbd94c7f65bc8e511de4b01570f7267c5
Amrin002 <amrinlatuconsina@gmail.com>
2024-01-19 02:35:40 +0900

    "satu2"

--- app/src/main/java/com/deranaindonesia/derana/fragmentberanda/fiturderana/PetaBahasaFragment.kt
+++ app/src/main/java/com/deranaindonesia/derana/fragmentberanda/fiturderana/PetaBahasaFragment.kt
@@ -1,12 +1,8 @@
 package com.deranaindonesia.derana.fragmentberanda.fiturderana
 
-import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.deranaindonesia.derana.R
 
-class PetaBahasaFragment : Fragment(R.layout.fragment_petabahasa) {
+class PetaBahasaFragment : Fragment(R.layout.fragment_fitur_petabahasa) {
 
 }
