8c169c0abe49125abeb07165fe8af9617602abb0
Amrin002 <amrinlatuconsina@gmail.com>
2024-01-20 01:48:05 +0900

    "Kamus Bahasa"

--- app/src/main/java/com/deranaindonesia/derana/fragmentberanda/fiturderana/PetaBahasaFragment.kt
+++ app/src/main/java/com/deranaindonesia/derana/fragmentberanda/fiturderana/PetaBahasaFragment.kt
@@ -2,11 +2,15 @@ package com.deranaindonesia.derana.fragmentberanda.fiturderana
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
+
 import android.view.View
-import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+
 import com.deranaindonesia.derana.R
 
 class PetaBahasaFragment : Fragment(R.layout.fragment_petabahasa) {
+
+
 
 }
