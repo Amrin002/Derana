c2f95825f53bb8a93291228134e46aff11170606
Amrin002 <amrinlatuconsina@gmail.com>
2024-02-01 18:47:13 +0900

    "Revisi Code"

--- app/src/main/java/com/deranaindonesia/derana/fragmentsignup/FragmentSignupEmail.kt
+++ app/src/main/java/com/deranaindonesia/derana/fragmentsignup/FragmentSignupEmail.kt
@@ -7,28 +7,27 @@ import android.view.View
 
 import android.widget.EditText
 import com.deranaindonesia.derana.R
+import com.deranaindonesia.derana.databinding.FragmentSignupEmailBinding
 import com.deranaindonesia.derana.datasignup.EmailDataSignup
 
 class FragmentSignupEmail : Fragment(R.layout.fragment_signup_email) {
-    private lateinit var etEmail: EditText
-    private lateinit var etFullName: EditText
-    private lateinit var etPassword: EditText
+
+    private var _binding : FragmentSignupEmailBinding? = null
+    private val binding get() = _binding!!
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentSignupEmailBinding.bind(view)
 
-        // Inisialisasi EditText di sini
-        etEmail = view.findViewById(R.id.etEmailSignup)
-        etFullName = view.findViewById(R.id.etNamaSignupEmail)
-        etPassword = view.findViewById(R.id.etPassSignupEmail)
+
     }
 
     fun getEmailValue(): EmailDataSignup? {
         //persiapan pengirimin nilai ke SignupActivity
         // Pindahkan pengambilan nilai ke sini
-        val email = etEmail.text.toString()
-        val efullname = etFullName.text.toString()
-        val epassword = etPassword.text.toString()
+        val email = binding.etEmailSignup.text.toString()
+        val efullname = binding.etNamaSignupEmail.text.toString()
+        val epassword = binding.etPassSignupEmail.text.toString()
 
         if (email.isNotEmpty() && efullname.isNotEmpty() && epassword.isNotEmpty()) {
             return EmailDataSignup(email, efullname, epassword)
